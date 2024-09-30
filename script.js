081b3cb7150314deb7a3677a709f9d30fe712bc8
Chau Luu <68700957+chauluu00@users.noreply.github.com>
2024-09-30 14:05:56 -0700

    Add working buttons that make calculations

--- script.js
+++ script.js
@@ -1,43 +1,88 @@
-let display = document.querySelector("output");
-let storedValue = "";
+let firstOperand = null;
+let secondOperand = null;
+let firstOperator = null;
+let displayValue = "0";
+let result = null;
 
-function add (a,b){
-    return a + b;
+const display = document.querySelector("output");
+function updateDisplay () {
+    display.value = displayValue;
 }
 
-function subtract (a,b){
-    return a - b;
-}
-
-function multiply (a,b){
-    return a * b;
-}
-
-function divide (a,b){
-    return a / b;
-}
+updateDisplay();
 
 //Populate the display when clicking number buttons
-const keypad = document.querySelector(".keypad");
-const numKeys = keypad.querySelectorAll(".number-key");
+const numKeys = document.querySelectorAll(".number-key");
 numKeys.forEach((numKey) => {
     numKey.addEventListener("click", ()=> {
-        display.value = `${numKey.textContent}`;
-        storedValue += display.value;
-        display.value = storedValue;
+        getOperand(numKey.textContent);
+        updateDisplay();
     });
 });
 
+function getOperand (operand){
+    if (displayValue === "0" || displayValue === firstOperand || displayValue === secondOperand) {
+        displayValue = operand;
+    } else {
+        displayValue += operand;
+    }    
+}
+
+//When an operator key is clicked, run getOperator
+const opKeys = document.querySelectorAll(".operator");
+opKeys.forEach((opKey) => {
+    opKey.addEventListener("click", ()=> {
+        getOperator(opKey.textContent);
+        updateDisplay();
+    });
+});
+
+//if the operator button clicked is = and firstOperator is null, do nothing 
+//if the operator button clicked is = and firstOperator is not null, operate but don't assign = as firstOperator
+function getOperator (operator){
+    if (firstOperator === null){
+        console.log(firstOperand = displayValue);
+        console.log(firstOperator = operator);
+    } else {
+        console.log(secondOperand = displayValue);
+        result = operate(Number(firstOperand), Number(secondOperand), firstOperator);
+        displayValue = result;
+        console.log(firstOperand = result);
+        console.log(firstOperator = operator);
+    }    
+}
+
 //Backspace button deletes the last character of string
-let backspace = document.querySelector("#backspace");
+const backspace = document.querySelector("#backspace");
 backspace.addEventListener("click", ()=>{
-    storedValue = storedValue.slice(0,-1);
-    display.value = storedValue;
-})
+    displayValue = displayValue.toString().slice(0,-1);
+    updateDisplay();
+});
 
 //Clear button
-let clear = document.querySelector("#clear");
+const clear = document.querySelector("#clear");
 clear.addEventListener("click", ()=>{
-    display.value = ""; //clear display
-    storedValue = ""; //clear stored value
-})
+    displayValue = "0";
+    updateDisplay();
+    firstOperand = null;
+    secondOperand = null;
+    firstOperator = null;
+    secondOperator = null;
+    result = null;
+});
+
+function operate (x, y, op){
+    if (op === "+"){
+        return x + y;
+    } else if (op === "-") {
+        return x - y;
+    } else if (op === "×") {
+        return x * y;
+    } else if (op === "÷") {
+        if (y === 0){
+            return "NA";
+        } else {
+            return x / y;
+        }
+    };
+}
